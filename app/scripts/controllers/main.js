f0d6d240d3f90156086a3577cfbed9d08b9c3bd2
Thomas Smith <tom.smith@imulus.com>
2013-08-11 13:40:50 -0600

    Pull in and render a games players

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('pongApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+
+    var host = 'localhost:3000/';
+
+    $http.get('http://localhost:3000/games.json').success( function(data) {
+      console.log(data);
+      $scope.games = data;
+    });
+
+
   });
